53b1bbfc0a39547f75aa83ec3cf09cfa9238918e
bnelsonibm <bnelsonibm@users.noreply.github.com>
2021-04-19 16:25:26 -0500

    Update ibmcloud_new_vpc_scale_variables.tf

--- ibmcloud_scale_templates/ibmcloud_new_vpc_scale/ibmcloud_new_vpc_scale_variables.tf
+++ ibmcloud_scale_templates/ibmcloud_new_vpc_scale/ibmcloud_new_vpc_scale_variables.tf
@@ -138,6 +138,5 @@ variable "instances_ssh_user" {
 
 variable "instances_ssh_private_key" {
   type        = string
-  sensitive   = true
   description = "SSH private key, which will be used to login to bastion host."
 }
